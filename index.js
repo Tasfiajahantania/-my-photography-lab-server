7d19a8631af59566ccb38fe344027f35f0320aab
Tasfiajahan <tania.develpers@gmail.com>
2022-11-09 17:01:05 +0600

    my third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -31,6 +31,12 @@ async function run() {
       const cursor = serviceCollection.find(query);
       const servicesAll = await cursor.toArray();
       res.send(servicesAll);
+    });
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     });
   } finally {
   }
