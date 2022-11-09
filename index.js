089c8cbcfdeb2e28e180c88f8629096be543c1fd
Tasfiajahan <tania.develpers@gmail.com>
2022-11-10 01:24:37 +0600

    5th commit

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("photography").collection("services");
+    const reviewCollection = client.db("photography").collection("review");
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -37,6 +38,11 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
       res.send(service);
+    });
+    app.post("/add-review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
     });
   } finally {
   }
